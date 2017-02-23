cd92dd2d800dad341a8000a78aff540fc25de51c
Chaoran Chen <Chaoran Chen>
2017-02-23 04:55:46 -0600

    p2 ?

---
+++ hw_q4/p1.r
@@ -0,0 +1,24 @@
+interAB 1 r1pAB 1 r2pAB 1 r1 1 r2 1 : x 1 interAB 1 : 100
+r1 1 r2AB 1 x 1 : r2p 1 r1 1 : 1000
+r2 1 r1AB 1 x 1 : r1p 1 r2 1 : 1000
+r2p 1 xAB 1 : r2 1 inter 1 : 1000
+r1p 1 xAB 1 : r1 1 inter 1 : 1000
+inter 1 r2pAB 1 : : 10000
+inter 1 r1pAB 1 : : 10000
+r1AB 1 r2AB 1 x 1 : r3 1 : 1000
+: r1AB 1 : 0.1
+r1 1 r1AB 1 : r1 1 : 100000000
+r1AB 2 : r1AB 1 : 100
+: r2AB 1 : 0.1
+r2 1 r2AB 1 : r2 1 : 100000000
+r2AB 2 : r2AB 1 : 100
+: r1pAB 1 : 0.1
+r1p 1 r1pAB 1 : r1p 1 : 100000000
+r1pAB 2 : r1pAB 1 : 100
+: r2pAB 1 : 0.1
+r2p 1 r2pAB 1 : r2p 1 : 100000000
+r2pAB 2 : r2pAB 1 : 100
+: xAB 1 : 0.1
+x 1 xAB 1 : x 1 : 100000000
+: interAB 1 : 0.1
+inter 1 interAB 1 : inter 1 : 100000000
---
+++ hw_q4/p2.r
@@ -0,0 +1,23 @@
+: r2AB 1 : 0.1
+r2 1 r2AB 1 : r2 1 : 1000000
+r2AB 2 : r2AB 1 : 100
+: pAB 1 : 0.1
+p 1 pAB 1 : p 1 : 100000000
+pAB 2 : pAB 1 : 100
+: r1AB 1 : 0.1
+r1 1 r1AB 1 : r1 1 : 100000000
+r1AB 2 : r1AB 1 : 100
+: r1pAB 1 : 0.1
+r1p 1 r1pAB 1 : r1p 1 : 100000000000
+r1pAB 2 : r1pAB 1 : 100
+: xAB 1 : 0.1
+x 1 xAB 1 : x 1 : 100000
+xAB 2 : xAB 1 : 1000
+r2AB 1 p 1 : r2AB 1 : 100
+p 10 : p 1 : 1000
+pAB 1 r1 2 : r1p 1 pAB 1 : 500
+pAB 1 r1 1 : x 1 pAB 1 : 1
+xAB 1 r1AB 1 r1p 1 : r2 1 xAB 1 r1AB 1 : 500
+x 1 r1p 1 : r2 6 x 1 : 1000
+x 1 r1pAB 1 : r2 4 : 500
+r1pAB 1 xAB 1 r2 1 : r1 1 p 1 r1pAB 1 xAB 1 : 10000
