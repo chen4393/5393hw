d2d840e6798a64598a87fb23b947cc9965a67e66
Chaoran Chen <Chaoran Chen>
2017-02-23 13:21:40 -0600

    final

--- hw_q4/p1.r
+++ hw_q4/p1.r
@@ -1,24 +1,24 @@
-interAB 1 r1pAB 1 r2pAB 1 r1 1 r2 1 : x 1 interAB 1 : 100
-r1 1 r2AB 1 x 1 : r2p 1 r1 1 : 1000
-r2 1 r1AB 1 x 1 : r1p 1 r2 1 : 1000
-r2p 1 xAB 1 : r2 1 inter 1 : 1000
-r1p 1 xAB 1 : r1 1 inter 1 : 1000
-inter 1 r2pAB 1 : : 10000
-inter 1 r1pAB 1 : : 10000
-r1AB 1 r2AB 1 x 1 : r3 1 : 1000
-: r1AB 1 : 0.1
-r1 1 r1AB 1 : r1 1 : 100000000
-r1AB 2 : r1AB 1 : 100
-: r2AB 1 : 0.1
-r2 1 r2AB 1 : r2 1 : 100000000
-r2AB 2 : r2AB 1 : 100
-: r1pAB 1 : 0.1
-r1p 1 r1pAB 1 : r1p 1 : 100000000
-r1pAB 2 : r1pAB 1 : 100
-: r2pAB 1 : 0.1
-r2p 1 r2pAB 1 : r2p 1 : 100000000
-r2pAB 2 : r2pAB 1 : 100
+a 1 b 1 swapAB 1 apAB 1 bpAB 1 : x 1 swapAB 1 : 100
+a 1 bAB 1 x 1 : bp 1 a 1 : 1000
+b 1 aAB 1 x 1 : ap 1 b 1 : 1000
+bp 1 xAB 1 : b 1 swap 1 : 1000
+ap 1 xAB 1 : a 1 swap 1 : 1000
+swap 1 bpAB 1 : : 10000
+swap 1 apAB 1 : : 10000
+aAB 1 bAB 1 x 1 : c 1 : 1000
+: aAB 1 : 0.1
+a 1 aAB 1 : a 1 : 100000000
+aAB 2 : aAB 1 : 100
+: bAB 1 : 0.1
+b 1 bAB 1 : b 1 : 100000000
+bAB 2 : bAB 1 : 100
+: apAB 1 : 0.1
+ap 1 apAB 1 : ap 1 : 100000000
+apAB 2 : apAB 1 : 100
+: bpAB 1 : 0.1
+bp 1 bpAB 1 : bp 1 : 100000000
+bpAB 2 : bpAB 1 : 100
 : xAB 1 : 0.1
 x 1 xAB 1 : x 1 : 100000000
-: interAB 1 : 0.1
-inter 1 interAB 1 : inter 1 : 100000000
+: swapAB 1 : 0.1
+swap 1 swapAB 1 : swap 1 : 100000000
